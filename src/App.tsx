56159425d6ba22c43dace897e885ee7770a8f3ff
soonki-98 <alstnsrl98@gmail.com>
2023-11-21 09:23:36 +0900

    feat: 퀴즈 풀기 전 선택사항 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  TextField,
+  Typography,
 } from "@mui/material";
 
 import "./App.css";
 import { Column } from "./components";
 import { getCategories } from "./apis/quiz";
+import { quizDifficulty, quizType } from "./constant";
 
 function App() {
   const { data } = useQuery({
@@ -23,11 +26,29 @@ function App() {
         <Column
           verticalAlign="center"
           horizonAlign="center"
-          gap={8}
-          style={{ width: "100%", height: "100%" }}
+          gap={16}
+          style={{
+            width: "100%",
+            maxWidth: "600px",
+            height: "100%",
+            margin: "0 auto",
+          }}
         >
-          <FormControl>
-            <InputLabel id="category-select-label">Select Category</InputLabel>
+          <Typography variant="h3" component="h1">
+            Solve Quiz
+          </Typography>
+          {/** 문제의 갯수 */}
+          <FormControl fullWidth>
+            <TextField
+              defaultValue={10}
+              variant="outlined"
+              label="Number of Questions"
+            />
+          </FormControl>
+
+          {/** 문제 카테고리 */}
+          <FormControl fullWidth>
+            <InputLabel id="category-select-label">Category</InputLabel>
             <Select
               defaultValue={data?.trivia_categories[0].id}
               label="Category"
@@ -39,6 +60,32 @@ function App() {
               ))}
             </Select>
           </FormControl>
+
+          {/** 문제 난이도*/}
+          <FormControl fullWidth>
+            <InputLabel id="difficulty-select-label">Difficulty</InputLabel>
+            <Select defaultValue="any" label="Category">
+              {quizDifficulty.map((category) => (
+                <MenuItem key={category.id} value={category.value}>
+                  {category.id}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          {/** 문제 풀이 종류*/}
+          <FormControl fullWidth>
+            <InputLabel id="type-select-label">Type</InputLabel>
+            <Select defaultValue="any" label="Type">
+              {quizType.map((category) => (
+                <MenuItem key={category.id} value={category.value}>
+                  {category.id}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          {/** 문제풀이 시작 버튼*/}
           <Button variant="contained">Solve Quiz</Button>
         </Column>
       </Column>
