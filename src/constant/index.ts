67252069697fd60ca0cbd81d5a55fbac636a3576
soonki-98 <alstnsrl98@gmail.com>
2023-11-21 09:22:45 +0900

    feat: 퀴즈 관련 상수 추가

---
+++ src/constant/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Created on Tue Nov 21 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+export * from "./quiz";
---
+++ src/constant/quiz.ts
@@ -0,0 +1,18 @@
+/*
+ * Created on Tue Nov 21 2023
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+export const quizDifficulty = [
+  { id: "Any Difficulty", value: "any" },
+  { id: "Easy", value: "easy" },
+  { id: "Medium", value: "medium" },
+  { id: "Hard", value: "hard" },
+];
+
+export const quizType = [
+  { id: "Any Type", value: "any" },
+  { id: "Multiple Choice", value: "multiple" },
+  { id: "True or False", value: "boolean" },
+];
