d938d63b834e75cae00b346e145608a3efcbd076
soonki-98 <alstnsrl98@gmail.com>
2023-11-23 17:24:00 +0900

    feat: 퀴즈 풀기 ㅍ페이지

--- src/pages/Solve.tsx
+++ src/pages/Solve.tsx
@@ -5,14 +5,31 @@
  */
 
 import { useQuery } from "@tanstack/react-query";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { getQuiz } from "../apis/quiz";
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
 
-// import { useQuery } from "@tanstack/react-query";
+import { getQuiz } from "../apis/quiz";
+import { Column, Row } from "../components";
+
+function shuffle<T>(array: T[]) {
+  return array.sort(() => Math.random() - 0.5);
+}
 
 export default function Solve() {
   const [searchParams] = useSearchParams();
+  const [activeStep, setActiveStep] = useState(0);
 
   const solveQueries = useMemo(() => {
     const result: Record<string, string> = {};
@@ -29,6 +46,82 @@ export default function Solve() {
       return getQuiz({ amount, category, difficulty, type });
     },
   });
-  console.log(":::::", data?.results);
-  return <div>문제풀기</div>;
+
+  if (!data) return null;
+  return (
+    <Column style={{ width: "100vw", height: "100vh" }}>
+      <Column
+        verticalAlign="center"
+        horizonAlign="center"
+        gap={16}
+        style={{
+          width: "100%",
+          maxWidth: "600px",
+          height: "100%",
+          margin: "0 auto",
+        }}
+      >
+        <Stepper activeStep={activeStep}>
+          {data?.results.map((quiz, index) => {
+            return (
+              <Step
+                key={index}
+                style={{ display: activeStep !== index ? "none" : undefined }}
+              >
+                <StepLabel color="red" style={{ margin: "20px 0" }}>
+                  <Typography variant="h6"> {quiz.category}</Typography>
+                </StepLabel>
+
+                <Column horizonAlign="center">
+                  <FormControl>
+                    <FormLabel>
+                      <Typography variant="body1" color="CaptionText">
+                        {quiz.question}
+                      </Typography>
+                    </FormLabel>
+                    <RadioGroup row>
+                      {shuffle([
+                        ...quiz.incorrect_answers,
+                        quiz.correct_answer,
+                      ]).map((answer) => {
+                        return (
+                          <FormControlLabel
+                            value={answer}
+                            key={answer}
+                            control={<Radio />}
+                            label={answer}
+                          />
+                        );
+                      })}
+                    </RadioGroup>
+                  </FormControl>
+
+                  <Row gap={10}>
+                    <Typography
+                      variant="h6"
+                      color={
+                        quiz.difficulty === "easy"
+                          ? "green"
+                          : quiz.difficulty === "medium"
+                            ? "orange"
+                            : "red"
+                      }
+                    >
+                      {quiz.difficulty}
+                    </Typography>
+                  </Row>
+                </Column>
+              </Step>
+            );
+          })}
+        </Stepper>
+        <Button
+          onClick={() => setActiveStep(activeStep + 1)}
+          variant="contained"
+        >
+          {activeStep < data.results.length - 1 ? "Solve Next" : "Submit"}
+        </Button>
+      </Column>
+    </Column>
+  );
 }
