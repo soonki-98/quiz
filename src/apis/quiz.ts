bcde84c1a952ee2c75d14b1ad37c4319e3776462
soonki-98 <alstnsrl98@gmail.com>
2023-11-23 14:45:33 +0900

    feat: 퀴즈 가져오기 api 추가

--- src/apis/quiz.ts
+++ src/apis/quiz.ts
@@ -1,8 +1,44 @@
 import { instance } from "./instance";
 
+type Quiz = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: "multiple" | "boolean";
+};
+
 export const getCategories = async () => {
   const result = await instance.get<{
     trivia_categories: { id: number; name: string }[];
   }>("/api_category.php");
+  return result.data;
+};
+
+export const getQuiz = async ({
+  amount,
+  category,
+  difficulty,
+  type,
+}: {
+  amount: string;
+  category: string;
+  difficulty: string;
+  type: string;
+}) => {
+  let path = `?amount=${amount}`;
+  if (category && category !== "any") {
+    path += `&category=${category}`;
+  }
+  if (difficulty && difficulty !== "any") {
+    path += `&difficulty=${difficulty}`;
+  }
+  if (type && type !== "any") {
+    path += `&type=${type}`;
+  }
+  const result = await instance.get<{ response_code: number; results: Quiz[] }>(
+    `api.php${path}`
+  );
   return result.data;
 };
